0e495e9d6d1217e8727ff58a04edfbe11979b094
darqus <voladores@inbox.ru>
2022-10-05 00:15:28 +0300

    add Account Page

---
+++ src/pages/AccountPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <CounterComponent />
+    AccountPage
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import CounterComponent from 'components/CounterComponent.vue'
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,4 @@
-import { RouteRecordRaw } from 'vue-router';
+import { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -6,13 +6,22 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+  {
+    path: '/account',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/AccountPage.vue') }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('layouts/MainLayout.vue'),
+    // component: () => import('pages/ErrorNotFound.vue'),
+    children: [
+      { path: '', component: () => import('pages/ErrorNotFound.vue') },
+    ],
   },
-];
+]
 
-export default routes;
+export default routes
