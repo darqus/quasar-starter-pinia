1ea63cc48b6c9a97032962e9fd7c6f602342dbdf
darqus <voladores@inbox.ru>
2022-10-05 00:13:59 +0300

    add RouterMenuLink

---
+++ src/components/RouterMenuLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-item clickable :to="path">
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+export interface RouterMenuLinkProps {
+  title: string
+  path?: string
+  icon?: string
+}
+withDefaults(defineProps<RouterMenuLinkProps>(), {
+  path: '/',
+  icon: '',
+})
+</script>
