a9abf95e8ed7d3a3ce719833e7294dfba5304b7a
darqus <voladores@inbox.ru>
2022-10-05 02:03:48 +0300

    fix usage store

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>Counter: {{ storeCounter.$state.counter }}</p>
+    <p>Counter: {{ storeCounter.counter }}</p>
     <div class="flex q-px-md">
       <div class="row justify-center items-center content-center">
         <q-btn color="primary" label="+" @click="storeCounter.increment" />
